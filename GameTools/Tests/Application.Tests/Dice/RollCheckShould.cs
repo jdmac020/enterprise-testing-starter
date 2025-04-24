fbe861f589746ec0abe143f8c851b8c18da06130
John MacGregor <john@orangeboyinc.com>
2025-04-24 20:31:04 +0000

    Add test project

---
+++ GameTools/Tests/Application.Tests/Dice/RollCheckShould.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Application.Tests.Dice
+{
+    [TestClass]
+    public class RollCheckShould
+    {
+        [TestMethod]
+        public void ReturnTrue()
+        {
+            false.ShouldBeTrue();
+        }
+    }
+}
---
+++ GameTools/Tests/Application.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Application.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Application.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("335c4240-dd82-47fc-b3a0-91cf3953dd68")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
